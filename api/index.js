8646a15d12c8797d480ad77b09b331f54221f443
FacundoFuentes <facufuentes5335@gmail.com>
2021-10-14 09:40:22 -0300

    Last Commit

--- api/index.js
+++ api/index.js
@@ -18,11 +18,24 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Genre } = require('./src/db.js');
+const axios = require("axios");
+
+async function loadGenres() {
+  const API_GENRES = await axios.get('https://api.rawg.io/api/genres?key=793cddd48b8c4276aa490eba102602e0')
+  const GENRES_NAME = API_GENRES.data.results.map((genre) => {
+      return {
+          name: genre.name
+      }
+  })
+  Genre.bulkCreate(GENRES_NAME)
+  console.log('Generos cargados correctamente')
+}
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('Server listening at 3001'); // eslint-disable-line no-console
+    loadGenres();
   });
 });
